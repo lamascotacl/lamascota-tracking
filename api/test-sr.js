5e3914f930972b03b4d4687c6c4db2b087c580ef
lamascotacl <lamascotamkt@gmail.com>
2025-12-11 16:57:07 -0300

    Update test-sr.js

--- api/test-sr.js
+++ api/test-sr.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     const response = await fetch(url, {
       method: "GET",
       headers: {
-        "Authorization": `Bearer ${token}`,
+        "Authorization": `Token ${token}`,
         "Accept": "application/json"
       }
     });
